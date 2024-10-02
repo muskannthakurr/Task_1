5ee47ee058cf8d245d564a95080b9af861712c0d
muskannthakurr <168154098+muskannthakurr@users.noreply.github.com>
2024-10-02 18:22:43 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,15 @@
+print("Simple Calculator")
+num1 = int(input("Enter first number :"))
+operator = input("Enter the desired operator(-,+,*,/) :")
+num2 = int(input("Enter secound number :"))
+
+if operator == "-":
+  print("Substraction of num1 and num2 :",num1 - num2)
+elif operator == "+":
+  print("Addition of num1 and num2 :",num1 + num2)
+elif operator == "*":
+  print("Multiplication of num1 and num2 :",num1 * num2)
+elif operator == "/":
+  print("Division of num1 and num2 :",num1 / num2)
+else:
+  print("Enter the right operator")
